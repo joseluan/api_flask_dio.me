b312defd96dcdbead8f577e61fdaac3937fc2311
José Luan <joseluan3301@gmail.com>
2022-10-23 17:04:46 -0300

    First commit

---
+++ api-developer-restful/app.py
@@ -0,0 +1,69 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+
+app = Flask(__name__)
+api = Api(app)
+
+developers = [
+    {
+        'name': 'José Luan',
+        'skills': ['Python', 'Flask']
+    },
+    {
+        'name': 'Maria da silva',
+        'skills': ['Java', 'Spring']
+    }
+]
+
+class Developer(Resource):
+    
+    def get(self, id):
+        try:
+            response =  developers[id]
+        except IndexError:
+            response = {
+                'status': 'erro',
+                'mesage': "Developer not found"
+            }
+        except Exception:
+            response = {
+                'status': 'erro',
+                'mesage': "Mysterious error"
+            }
+
+
+        return response
+    def put(self, id):
+        developers[id] = request.get_json()
+        return {
+            'status': 'ok',
+            'mesage': "Developer update with success"
+        }
+    def delete(self, id):
+        developers.pop(id)
+        return {
+            'status': 'ok',
+            'mesage': "Developer removed with success"
+        }
+
+class ListDevelopers(Resource):
+
+    def get(self):
+        return developers
+    
+    def post(self):
+        pos = len(developers)
+        developers.append(request.get_json())
+        return {
+            'status': 'ok',
+            'mesage': "Developer create with success",
+            'id': pos
+        }        
+
+
+api.add_resource(Developer, '/dev/<int:id>')
+api.add_resource(ListDevelopers, '/dev/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ api-developer/app.py
@@ -0,0 +1,64 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+developers = [
+    {
+        'name': 'José Luan',
+        'skills': ['Python', 'Flask']
+    },
+    {
+        'name': 'Maria da silva',
+        'skills': ['Java', 'Spring']
+    }
+]
+
+@app.route('/dev/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def developer(id):
+    if request.method == 'GET':    
+        try:
+            response =  jsonify(developers[id])
+        except IndexError:
+            response = jsonify({
+                'status': 'erro',
+                'mesage': "Developer not found"
+            })
+        except Exception:
+            response = jsonify({
+                'status': 'erro',
+                'mesage': "Mysterious error"
+            })
+
+
+        return response
+    elif request.method == 'PUT':
+        developers[id] = request.get_json()
+        return jsonify({
+            'status': 'ok',
+            'mesage': "Developer update with success"
+        })
+    elif request.method == 'DELETE':
+        developers.pop(id)
+        return jsonify({
+            'status': 'ok',
+            'mesage': "Developer removed with success"
+        })    
+
+
+
+@app.route('/dev/', methods=['GET', 'POST'])
+def list_developers():
+    if request.method == 'GET':
+        return jsonify(developers)
+    elif request.method == 'POST':
+        pos = len(developers)
+        developers.append(request.get_json())
+        return jsonify({
+            'status': 'ok',
+            'mesage': "Developer create with success",
+            'id': pos
+        })
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
